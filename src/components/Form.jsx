58dea4fcee6756c47e48330afdc8d8579dab1c0d
Soohyun Lim <d2axasap@gmail.com>
2023-09-20 11:52:00 -0400

    Finish adding styles

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,17 +1,16 @@
 const Form = ({ name, setName, handleSubmit}) => {
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor="name">
-        Name
-        <input
-          type="text"
-          name="name"
-          id="name"
-          required={true}
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </label>
+      <label htmlFor="name">Name</label>
+      <input
+        type="text"
+        name="name"
+        id="name"
+        required={true}
+        autoComplete="off"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
       <button>Submit</button>
     </form>
   );
